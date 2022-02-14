0a3df152e65d8b4babe840059876efebfd2a7844
Roman Holub <warior210@gmail.com>
2022-02-14 02:40:01 +0200

    path fixes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,8 +8,8 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
+
   heroes!: Hero[];
-  selectedHero!: Hero;
 
   constructor(private heroService: HeroService) {
   }
@@ -17,11 +17,7 @@ export class HeroesComponent implements OnInit {
   ngOnInit(): void {
     this.getHeroes();
   }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
+  
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
